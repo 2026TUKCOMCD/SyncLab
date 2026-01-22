23b195f8de18cad2c283004f8cddd45c2eb3a8e0
devwooks <seonguk8631@gmail.com>
2026-01-22 12:21:14 +0900

    데이터베이스 스키마 수정

--- DataBase_Schema.sql
+++ DataBase_Schema.sql
@@ -21,7 +21,7 @@ SET @@SESSION.SQL_LOG_BIN= 0;
 -- GTID state at the beginning of the backup 
 --
 
-SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ 'd7bc9580-bfad-11f0-80c4-f00132f84673:1-73';
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ 'd7bc9580-bfad-11f0-80c4-f00132f84673:1-88';
 
 --
 -- Table structure for table `edit`
@@ -126,17 +126,20 @@ LOCK TABLES `user_session` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Video`
+-- Table structure for table `video`
 --
 
-DROP TABLE IF EXISTS `Video`;
+DROP TABLE IF EXISTS `video`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Video` (
+CREATE TABLE `video` (
   `video_id` int NOT NULL AUTO_INCREMENT,
   `session_session_id` int NOT NULL,
   `s3_url` varchar(300) NOT NULL,
   `upload_status` varchar(10) NOT NULL,
+  `absoulte_start_time` bigint NOT NULL,
+  `absoulte_end_time` bigint NOT NULL,
+  `duration` double NOT NULL,
   PRIMARY KEY (`video_id`),
   UNIQUE KEY `s3_url_UNIQUE` (`s3_url`),
   KEY `fk_Video_session1_idx` (`session_session_id`),
@@ -145,12 +148,12 @@ CREATE TABLE `Video` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Video`
+-- Dumping data for table `video`
 --
 
-LOCK TABLES `Video` WRITE;
-/*!40000 ALTER TABLE `Video` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Video` ENABLE KEYS */;
+LOCK TABLES `video` WRITE;
+/*!40000 ALTER TABLE `video` DISABLE KEYS */;
+/*!40000 ALTER TABLE `video` ENABLE KEYS */;
 UNLOCK TABLES;
 SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -163,4 +166,4 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-01-21 16:48:05
+-- Dump completed on 2026-01-22 12:19:01
